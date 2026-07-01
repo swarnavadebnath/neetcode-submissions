a505cbb4b542878b69c2367db7a0dbe34aae7b05
Swarnava Debnath <swarnava.debnath08@gmail.com>
2026-07-01 21:25:21 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col,pd,nd = set(),set(),set()
+        res = []
+        board = [["."] * n for _ in range(n)]
+        def dfs(r):
+            if r==n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            for c in range(n):
+                if c in col or (r-c) in pd or (r+c) in nd:
+                    continue
+                col.add(c)
+                pd.add(r-c)
+                nd.add(r+c)
+                board[r][c] = "Q"
+                dfs(r+1)
+                col.remove(c)
+                pd.remove(r-c)
+                nd.remove(r+c)
+                board[r][c] = "."
+        dfs(0)
+        return len(res)
