369d3b426894aaf1ee54b7d616dac91b052609ff
Swarnava Debnath <swarnava.debnath08@gmail.com>
2026-07-05 16:43:47 +0530

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = defaultdict(int)
+        for a,b in trust:
+            count[a]-=1
+            count[b]+=1
+        for i in range(1,n+1):
+            if count[i] == n-1:
+                return i
+        return -1
+        
