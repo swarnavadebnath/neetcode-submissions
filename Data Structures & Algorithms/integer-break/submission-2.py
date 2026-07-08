c5e8a81e4e4d82e6848260091e1674564ec26701
Swarnava Debnath <swarnava.debnath08@gmail.com>
2026-07-09 00:47:24 +0530

    Add: integer-break - submission-2

---
+++ Data Structures & Algorithms/integer-break/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n==3:
+            return 2
+        if n == 4:
+            return 4
+        q = n//3
+        r = n % 3
+        if r == 0:
+            return 3**q
+        elif r == 1:
+            return 3**(q-1)*4
+        else:
+            return ((3**q)*r)
+        
+        
+        
