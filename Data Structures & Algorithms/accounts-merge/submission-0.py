8d04debd61dab3b3676fbba27083eeb03b62dfbc
Swarnava Debnath <swarnava.debnath08@gmail.com>
2026-07-26 18:19:28 +0530

    Add: accounts-merge - submission-0

---
+++ Data Structures & Algorithms/accounts-merge/submission-0.py
@@ -0,0 +1,42 @@
+class UnionFind:
+    def __init__(self,n):
+        self.par = [i for i in range(n)]
+        self.rank = [1]*n
+    def find(self,x):
+        while x!= self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+    def union(self,x1,x2):
+        p1 = self.find(x1)
+        p2 = self.find(x2)
+        if p1 == p2:
+            return False
+        if self.rank[p1]>self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1]+=self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2]+=self.rank[p1]
+        return True
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        email = {}
+        for i,a in enumerate(accounts):
+            for e in a[1:]:
+                if e in email:
+                    uf.union(i,email[e])
+                else:
+                    email[e] = i
+        group = defaultdict(list)
+        for e,i in email.items():
+            l = uf.find(i)
+            group[l].append(e)
+        res = []
+        for i,em in group.items():
+            name = accounts[i][0]
+            res.append([name]+sorted(group[i]))
+        return res
+
+        
