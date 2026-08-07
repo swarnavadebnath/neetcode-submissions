3b453d7929c5d33dc329270c786f309ee1f6479f
GoofySwar <swarnava.debnath08@gmail.com>
2026-08-07 23:09:40 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        res = 0
+        for i in range(len(s)-1):
+                if s[i] == 'I'and s[i+1] in ('V','X'):
+                    res -= 1
+                elif s[i] == 'X' and s[i+1] in ('L','C'):
+                    res -= 10
+                elif s[i] == 'C' and s[i+1] in ('D','M'):
+                    res -= 100
+                else:
+                    res+=d[s[i]]
+        return (res+d[s[len(s)-1]])
+        
