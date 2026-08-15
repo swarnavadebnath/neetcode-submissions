264fef5f13ed751368f99e495d4bb66371cbafcc
GoofySwar <swarnava.debnath08@gmail.com>
2026-08-15 17:37:52 +0530

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m = len(s)
+        n = len(p)
+        dp = {}
+        def dfs(i,j):
+            if j == n:
+                return i == m
+            if (i,j) in dp:
+                return dp[(i,j)]
+            match = True if (i<m and (s[i]==p[j] or p[j]=='.')) else False
+            if (j+1)<n and p[j+1] == "*":
+                dp[(i,j)] = (dfs(i,j+2) or (match and dfs(i+1,j)))
+                return dp[(i,j)]
+            if match:
+                dp[(i,j)] = dfs(i+1,j+1)
+                return dp[(i,j)]
+            dp[(i,j)] = False
+            return False
+        return dfs(0,0)
+            
